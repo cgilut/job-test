110c10fa609408065115bf8052f4dbcd92a1212b
cgilut <117078690+cgilut@users.noreply.github.com>
2023-02-11 14:06:54 +0100

    Complete fourth test

---
+++ test-4.py
@@ -0,0 +1,32 @@
+# Задача 4
+# Написать метод/функцию, который/которая на вход принимает целое число, а на выходе возвращает то, 
+# является ли число простым (не имеет делителей кроме 1 и самого себя).
+
+def is_it_a_prime_number(integer_input):
+    return integer_input
+
+integer_input = None
+
+# Проверяем, является ли введенное значение числом, если нет, то выдаем ошибку и просим попробовать еще раз
+while integer_input is None:
+    user_input = input("Пожалуйста, введите целое, положительное число: ")
+    try:
+        integer_input = int(user_input)
+        # Проверка на положительное число
+        if integer_input < 1:
+            print("Ошибка, введенное число не является положительным.")
+            integer_input = None
+    except ValueError:
+        print("Ошибка, введенное значение не является целым числом.")
+
+k = 2
+
+while True:
+    if integer_input % k == 0 and integer_input != k:
+        print("Число " + str(integer_input) + " не является простым.")
+        break
+    if integer_input % k == 0 and integer_input == k:
+        print("Число " + str(integer_input) + " является простым.")
+        break
+    else:
+        k += 1
